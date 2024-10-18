c34a00e65c934e02987a1b2e9f0acacbe6c02b1e
leeheezin <gmlwls107@naver.com>
2024-10-18 17:54:25 +0900

    로그인

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,23 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import api from "../utils/api";
 
 const LoginPage = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+  const [user,setUser] = useState(null)
+  const navigate = useNavigate()
+
+  const handleLogin = async (e) => {
+    e.preventDefault()
+    try {
+      const res = await api.post('/user/login', { email, password })
+      if (res.status === 200) {
+        setUser(res.data.user)
+        sessionStorage.setItem("token",res.data.token)
+        api.defaults.headers["authorization"] = "Bearer "+res.data.token
+        setError('')
+        navigate('/')
+      }
+      throw new Error(res.data.error)
+    } catch (error) {
+      setError(error.message)
+    }
+  }
   return (
     <div className="display-center">
-      <Form className="login-box">
+      <Form className="login-box" onSubmit={handleLogin}>
         <h1>로그인</h1>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="email" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
         </Form.Group>
+        {error && <div>{error}</div>}
+
         <div className="button-box">
           <Button type="submit" className="button-primary">
             Login
