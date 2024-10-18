86d930e4bcb72f6f1e51b6754d816d50430f5977
leeheezin <gmlwls107@naver.com>
2024-10-18 11:54:57 +0900

    index.js수정

--- src/components/TodoBoard.js
+++ src/components/TodoBoard.js
@@ -5,7 +5,7 @@ const TodoBoard = ({todoList,delTask,upDateTask}) => {
   return (
     <div>
       <h2>Todo List</h2>
-      {todoList.length>0? todoList.map((item)=><TodoItem key={item._id} item={item} delTask={delTask} upDateTask={upDateTask}/>) : <h2>There is no items</h2>}
+      {todoList.length> 0? todoList.map((item)=><TodoItem key={item._id} item={item} delTask={delTask} upDateTask={upDateTask}/>) : <h2>There is no items</h2>}
       {/* <TodoItem/> will be here once we get the todoList */}
       <h2>There is no Item to show</h2>
     </div>
